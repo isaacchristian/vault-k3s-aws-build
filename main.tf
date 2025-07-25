8b148805190a01a7e29f4c42c7ed462bc47f39cf
isaacchristian <isaac.christian@github.com>
2025-07-25 13:48:26 -0400

    adding nginx yaml files for k3s

--- main.tf
+++ main.tf
@@ -15,13 +15,14 @@ module "vault-demo-vpc" {
   version = "5.1.1"
 
   name = "vault-demo-vpc"
-  cidr = "10.1.0.0/16"
+  cidr = var.cidr
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
   private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
   public_subnets  = ["10.1.11.0/24", "10.1.12.0/24"]
 
   enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_vpn_gateway   = false
   enable_dns_hostnames = true
 }
@@ -47,11 +48,25 @@ module "vault-security-group" {
       cidr_blocks = "0.0.0.0/0"
     },
     {
-      from_port   = 1389
-      to_port     = 1389
+      from_port   = 443
+      to_port     = 443
       protocol    = "tcp"
-      description = "LDAP port"
+      description = "Connect to Vault server (HTTPS)"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 30000
+      to_port     = 32767
+      protocol    = "tcp"
+      description = "Kubernetes NodePort range"
       cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "Allow SSH"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 
@@ -67,11 +82,57 @@ module "vault-security-group" {
   egress_rules       = ["https-443-tcp", "http-80-tcp"]
 }
 
+#Create Security Group for Nginx instance
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx-server-access"
+  description = "Allow connection to Nginx server"
+  vpc_id      = module.vault-demo-vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "Connect to Nginx server (HTTP)"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    description = "Connect to Nginx server (HTTPS)"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    description = "Kubernetes NodePort range"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    description = "Allow SSH"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    description = "Allow egress to everything within VPC"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 #Create Vault server EC2 instance with AWS Linux AMI
 resource "aws_instance" "vault-server" {
   ami           = data.aws_ami.aws_linux_hvm2.id
-  instance_type = "t3.micro"
+  instance_type = "t4g.micro"
 
   key_name                    = aws_key_pair.vault_key.key_name
   monitoring                  = true
@@ -81,5 +142,21 @@ resource "aws_instance" "vault-server" {
   user_data                   = templatefile("${path.module}/vault_user_data.tftpl", { vaultpass = random_string.vault_pass.id })
   tags = {
     Name = "vault-demo"
+  }
+}
+
+#Create Nginx server EC2 instance with AWS Linux AMI
+resource "aws_instance" "nginx-server" {
+  ami           = var.ami_type
+  instance_type = "t4g.large"
+
+  key_name                    = aws_key_pair.vault_key.key_name
+  monitoring                  = true
+  subnet_id                   = module.vault-demo-vpc.public_subnets[0]
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
+  user_data                   = templatefile("${path.module}/nginx_user_data.tftpl", {})
+  tags = {
+    Name = "nginx-demo"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,22 @@
 variable "region" {
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
   description = "The AWS region into which to deploy the HVN"
 }
 
 variable "public_key" {
   type        = string
+  default     = "vault-key.pub"
   description = "Public key to log into AWS instance"
+}
+
+variable "cidr" {
+  type    = string
+  default = "10.1.0.0/16"
+}
+
+variable "ami_type" {
+  type        = string
+  default     = "ami-022bbd2ccaf21691f"
+  description = "The type of AMI to use for the EC2 instances (e.g., amazon-linux-2, ubuntu-20.04)"
 }
